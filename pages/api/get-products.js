d1ca4cd83f7cfd533d9bf62c2edf16e3c34729df
RK-Son <son.nguyen@rikai.technology>
2021-05-14 22:52:16 +0700

    Test timeout

--- pages/api/get-products.js
+++ pages/api/get-products.js
@@ -1,9 +1,7 @@
 import {getProducts} from "../../utils/contentful";
 
 export default async (req, res) => {
-    const products = await getProducts();
-    products.forEach(product => {
-        console.log(product.fields)
-    })
-    res.status(200).json({products: products})
+    // const products = await getProducts({limit: 15});
+    // res.status(200).json({products: products})
+    await new Promise(resolve => setTimeout(() => res.status(200).json('Done'), 12000))
 }
